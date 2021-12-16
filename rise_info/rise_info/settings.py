19696392827eb8367944ebfc1f522520209671a3
higashiyama <higashiyama-k105r@mlit.go.jp>
2021-12-17 08:51:16 +0900

    make base.html

--- rise_info/rise_info/settings.py
+++ rise_info/rise_info/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # APPS
     'infos.apps.InfosConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'rise_info.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
