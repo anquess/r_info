f067fd150073f7ce757963b5d06bf79b4f22a593
higashiyama <higashiyama-k105r@mlit.go.jp>
2021-12-19 10:51:09 +0900

    markdownx remove

--- rise_info/infos/admin.py
+++ rise_info/infos/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from infos.models import Info
 
-from markdownx.admin import MarkdownxModelAdmin
-
-admin.site.register(Info, MarkdownxModelAdmin)
+admin.site.register(Info)
