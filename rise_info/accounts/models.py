93b5284df680798aec694e9f2118d1c6620e6fe6
higashiyama <higashiyama-k105r@mlit.go.jp>
2021-12-24 09:53:54 +0900

    change User password 暗号化

--- rise_info/accounts/models.py
+++ rise_info/accounts/models.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 
 def createUser(office):
     if User.objects.filter(username=office.slug).count() == 0:
-        User.objects.create(
+        user = User.objects.create(
             username=office.slug,
-            password=office.slug,
             first_name=office.name,
             last_name=office.shortcut_name,
         )
+        user.set_password(office.slug)
+        user.save()
