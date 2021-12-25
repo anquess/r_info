da6278567d19a4e14f79b8df149c486a812292ad
higashiyama <higashiyama-k105r@mlit.go.jp>
2021-12-25 15:19:13 +0900

    make CommonInfo model
    For Info FailuerReport model's abstract

--- rise_info/rise_info/baseModels.py
+++ rise_info/rise_info/baseModels.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.http import Http404
+from django_currentuser.db.models import CurrentUserField
+from django_currentuser.middleware import (get_current_authenticated_user)
 
 from datetime import datetime as dt
+
+from mdeditor.fields import MDTextField
+
 
 def csvFormatCheck(csvRow, checkLists) -> bool:
     for check in checkLists:
@@ -25,3 +30,24 @@ class BaseManager(models.Manager):
             return self.get_queryset().get(**kwargs)
         except self.model.DoesNotExist:
             return None
+
+class CommonInfo(models.Model):
+    objects = BaseManager()
+    title = models.CharField(verbose_name='タイトル', default="", null=False, blank=False, max_length=128)
+    sammary = models.TextField(verbose_name='概要', default="", null=False, blank=True, max_length=512)
+    content = MDTextField(verbose_name='内容', default="", null=False, blank=True)
+    created_by = CurrentUserField(verbose_name='登録者',on_update=True, related_name='%(app_label)s_%(class)s_create', null=False, blank=False)
+    created_at = models.DateTimeField(verbose_name='投稿日', auto_now_add=True, null=False, blank=False)
+    updated_at = models.DateTimeField(verbose_name='更新日', auto_now=True, null=False, blank=False)
+    updated_by = CurrentUserField(verbose_name='更新者', on_update=True, related_name='%(app_label)s_%(class)s_update', null=False, blank=False)
+    class Meta:
+        abstract= True
+
+    def __str__(self):
+        return self.title
+    
+    def save(self,obj, *args, **kwargs):
+        if not self.pk:
+            self.created_by = get_current_authenticated_user()
+        self.updated_by = get_current_authenticated_user()
+        super(CommonInfo, self).save(*args, **kwargs)
