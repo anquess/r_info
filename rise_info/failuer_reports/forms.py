6e4f776192eafed88194819dbac89cbd703bac0d
higashiyama <higashiyama-k105r@mlit.go.jp>
2021-12-26 14:30:14 +0900

    add impact

--- rise_info/failuer_reports/forms.py
+++ rise_info/failuer_reports/forms.py
@@ -5,4 +5,12 @@ from .models import FailuerReport
 class FailuerReportForm(forms.ModelForm):
     class Meta:
         model = FailuerReport
-        fields = ('title', 'sammary', 'content')
+        fields = (
+            'title',
+            'sammary',
+            'content', 
+            'is_operatinal_impact',
+            'operatinal_impact',
+            'is_flight_impact',
+            'flight_impact',
+             )
--- rise_info/failuer_reports/models.py
+++ rise_info/failuer_reports/models.py
@@ -4,6 +4,10 @@ from rise_info.baseModels import CommonInfo
 
 class FailuerReport(CommonInfo):
     sammary = models.TextField(verbose_name='概要', default="", null=False, blank=True, max_length=512)
+    is_operatinal_impact = models.BooleanField(verbose_name='運用への影響有無', default=False)
+    operatinal_impact = models.CharField(verbose_name='運用への影響', default="", null=False, blank=True, max_length=128)
+    is_flight_impact = models.BooleanField(verbose_name='運航への影響有無', default=False)
+    flight_impact = models.CharField(verbose_name='運航への影響', default="", null=False, blank=True, max_length=128)
 
     def save(self, *args, **kwargs):
         super(FailuerReport, self).save(self, *args, **kwargs)
