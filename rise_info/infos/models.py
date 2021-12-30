9248e836c88f817fff3fbd18214b5e233854fa37
higashiyama <higashiyama-k105r@mlit.go.jp>
2021-12-30 11:28:49 +0900

    Info models add infoAttachment

--- rise_info/infos/models.py
+++ rise_info/infos/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-from rise_info.baseModels import CommonInfo
+import os
+
+from rise_info.baseModels import CommonInfo, BaseManager
 
 class Info(CommonInfo):
     sammary = models.TextField(verbose_name='概要', default="", null=False, blank=True, max_length=512)
@@ -10,3 +12,11 @@ class Info(CommonInfo):
     
     class Meta:
         db_table = 'infos'
+
+class InfoAttachment(models.Model):
+    objects = BaseManager()
+    info = models.ForeignKey(Info , on_delete=models.CASCADE)
+    file = models.FileField(verbose_name='ファイル名', default='', null=False, blank=True, max_length=64)
+
+    class Meta:
+        db_table = 'info_attachments'
