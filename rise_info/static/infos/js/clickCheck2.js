df82ea385ad5594dbe135f9dffaa79a6c52977a6
higashiyama <higashiyama-k105r@mlit.go.jp>
2022-05-15 10:46:05 +0900

    del clickCheck2

--- rise_info/static/infos/js/clickCheck2.js
+++
@@ -1,7 +0,0 @@
-function clickCheck2(checked_id, text_id) {
-    if (document.getElementById(checked_id).checked === true) {
-        document.getElementById(text_id).type = 'text';
-    } else {
-        document.getElementById(text_id).type = 'hidden';
-    }
-}
