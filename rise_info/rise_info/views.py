9d77e185c60d34383459826a310615068f8a6743
higashiyama <higashiyama-k105r@mlit.go.jp>
2022-06-04 14:31:41 +0900

    新着情報機能追加

--- rise_info/rise_info/views.py
+++ rise_info/rise_info/views.py
@@ -1,7 +1,13 @@
+import imp
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 from accounts.views import addTmcAuth
+from infos.models import Info
+from contents.models import Contents
 
 
 @login_required
@@ -9,6 +15,13 @@ def top(request):
     context = addTmcAuth({}, request.user)
 #    ip = request.META.get('REMOTE_ADDR') # django toolbar
 #    context["IP"] = ip # django toolbar
+    info = Info.objects.filter(is_disclosed=True).filter(
+        disclosure_date__lte=datetime.today()).filter(
+        disclosure_date__gte=datetime.today() - relativedelta(months=24)).order_by('-disclosure_date')
+    contents = Contents.objects.filter(updated_at__gte=datetime.today(
+    ) - relativedelta(months=3)).order_by('-updated_at')
+    context['infos'] = info
+    context['contents'] = contents
     return render(request, "top.html", context)
 
 
