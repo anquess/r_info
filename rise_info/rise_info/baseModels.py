0e9b2b266c63d87eed3f6c0174baac7a77afaa67
higashiyama <higashiyama-k105r@mlit.go.jp>
2022-06-12 09:23:06 +0900

    fix maxlength

--- rise_info/rise_info/baseModels.py
+++ rise_info/rise_info/baseModels.py
@@ -40,9 +40,9 @@ class CommonInfo(models.Model):
     title = models.CharField(
         verbose_name='タイトル', default="", null=False, blank=False, max_length=128)
     content = models.TextField(
-        verbose_name='内容', default="", null=False, blank=True)
+        verbose_name='内容', default="", null=False, blank=True, max_length=4096)
     created_by = CurrentUserField(verbose_name='登録者', on_update=True,
-                                  related_name='%(app_label)s_%(class)s_create', null=False, blank=False, max_length=4096)
+                                  related_name='%(app_label)s_%(class)s_create', null=False, blank=False)
     created_at = models.DateTimeField(
         verbose_name='投稿日', auto_now_add=True, null=False, blank=False)
     updated_at = models.DateTimeField(
