7253ce8e0c6c8c826a245d9f2105c1f83e9e19c8
higashiyama <higashiyama-k105r@mlit.go.jp>
2022-08-04 14:32:58 +0900

    refactroring move FileSizeValidator
    from info.form to baseforms

--- rise_info/infos/forms.py
+++ rise_info/infos/forms.py
@@ -1,35 +1,11 @@
 from distutils.log import error
 from django import forms
 from django.urls import reverse_lazy
-from django.core.exceptions import ValidationError
-from django.core.files.uploadedfile import UploadedFile
 
 from infos.models import Info, AttachmentFile, InfoComments
-from rise_info.baseForms import MetaCommonInfo
+from rise_info.baseForms import MetaCommonInfo, FileSizeValidator
 from eqs.widgets import SuggestWidget
 from offices.widgets import OfficeSuggestWidget
-
-
-class FileSizeValidator:
-    def __init__(self, val: float, byte_type="mb"):
-        assert byte_type in ["b", "kb", "mb", "gb"]
-        if byte_type == "b":
-            self._upper_byte_size = val
-        elif byte_type == "kb":
-            self._upper_byte_size = 1000 * val
-        elif byte_type == "mb":
-            self._upper_byte_size = (1000 ** 2) * val
-        elif byte_type == "gb":
-            self._upper_byte_size = (1000 ** 3) * val
-        self._err_message = f"アップロードファイルは{val}{byte_type.upper()}未満にしてください"
-
-    def __call__(self, file_val: UploadedFile):
-        byte_size = file_val.size
-        if byte_size > self._upper_byte_size:
-            raise ValidationError(
-                code='file',
-                message=self._err_message
-            )
 
 
 class InfoCommentsForm(forms.ModelForm):
