b0ad9e4978e62c2109ccbc97ea9661e66f636013
higashiyama <higashiyama-k105r@mlit.go.jp>
2022-10-01 19:08:49 +0900

    add exportInfo

--- rise_info/infos/views.py
+++ rise_info/infos/views.py
@@ -7,6 +7,7 @@ from django.utils.decorators import method_decorator
 from django.db.models import Q
 
 from infos.models import Info, AttachmentFile, InfoComments, InfoTypeChoices
+from .exportInfo import makeInfoSheet
 from infos.forms import InfoForm, FileFormSet, InfoCommentsForm
 from accounts.views import isInTmcGroup, addTmcAuth
 from offices.models import Office
@@ -25,42 +26,6 @@ def exportInfo(request):
     makeInfoSheet(ws)
     wb.save(response)
     return response
-
-
-def makeInfoSheet(ws):
-    ws.title = 'info'
-    ws.cell(1, 1).value = 'Info_ID'
-    ws.cell(1, 2).value = '管理番号'
-    ws.cell(1, 3).value = '件名'
-    ws.cell(1, 4).value = '概要'
-    ws.cell(1, 5).value = '内容'
-    ws.cell(1, 6).value = 'InfoType_ID'
-    ws.cell(1, 7).value = '発行年月日'
-    ws.cell(1, 8).value = '登録者'
-    ws.cell(1, 9).value = '掲載'
-    ws.cell(1, 10).value = '投稿日'
-    ws.cell(1, 11).value = '更新日'
-    ws.cell(1, 12).value = '更新者'
-
-    row = 2
-    for info in Info.objects.all():
-        ws.cell(row, 1).value = info.pk
-        ws.cell(row, 2).value = info.managerID
-        ws.cell(row, 3).value = info.title
-        ws.cell(row, 4).value = info.sammary
-        ws.cell(row, 5).value = info.content
-        ws.cell(row, 6).value = info.info_type
-        ws.cell(row, 7).value = info.disclosure_date
-        if info.created_by:
-            user = info.created_by.username
-            ws.cell(row, 8).value = user
-        ws.cell(row, 9).value = info.is_disclosed
-        ws.cell(row, 10).value = info.created_at.replace(tzinfo=None)
-        ws.cell(row, 11).value = info.updated_at.replace(tzinfo=None)
-        if info.updated_by:
-            user = info.updated_by.username
-            ws.cell(row, 12).value = user
-        row += 1
 
 
 class InfoList(ListView):
