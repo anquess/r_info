dfa33eaed1e61625c6031af7f5ec136a929d5818
higashiyama <higashiyama-k105r@mlit.go.jp>
2023-02-28 10:21:26 +0900

    add filter 障害発生日

--- rise_info/infos/templatetags/custom_filters.py
+++ rise_info/infos/templatetags/custom_filters.py
@@ -9,6 +9,18 @@ def keyword(querydict):
 
     return "" if keyword is None else keyword
 
+@register.filter
+def date_min(querydict):
+    date_min = querydict.get('date_min')
+
+    return "" if date_min is None else date_min
+
+@register.filter
+def date_max(querydict):
+    date_max = querydict.get('date_max')
+
+    return "" if date_max is None else date_max
+
 
 @register.filter
 def eqtype(querydict):
@@ -22,7 +34,6 @@ def is_all(querydict):
     is_all = querydict.get('is_all')
 
     return "" if is_all is None else 'checked'
-
 
 @register.filter
 def remove_dirs(path):
